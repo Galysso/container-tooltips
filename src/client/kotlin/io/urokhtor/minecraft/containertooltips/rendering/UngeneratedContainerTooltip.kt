39b4a0de2b2ca8cb87adf147fc679aed1d63f728
Galysso <170312039+Galysso@users.noreply.github.com>
2025-04-16 00:45:43 +0200

    Changed color for ungenerated lootchests

--- src/client/kotlin/io/urokhtor/minecraft/containertooltips/rendering/UngeneratedContainerTooltip.kt
+++ src/client/kotlin/io/urokhtor/minecraft/containertooltips/rendering/UngeneratedContainerTooltip.kt
@@ -12,7 +12,7 @@ class UngeneratedContainerTooltip {
             textRenderer,
             listOf(
                 Text.of(container.name).asOrderedText(),
-                Text.translatable("container.ungenerated").withColor(Colors.GRAY).asOrderedText()
+                Text.translatable("container.ungenerated").withColor(Colors.LIGHT_RED).asOrderedText()
             ),
             DirectTooltipPositioner(),
             horizontalCenter - textRenderer.getWidth(container.name) / 2,
